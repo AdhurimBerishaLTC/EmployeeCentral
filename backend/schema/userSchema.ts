39ad55a3d7f9a1bebd6094c7a52a15ed2acc34e7
AdhurimBerishaLTC <adhurim.berisha@lowerthecurve.com>
2026-06-10 09:29:14 +0200

    createUser mutation

---
+++ backend/schema/userSchema.ts
@@ -0,0 +1,63 @@
+export const userTypeDefs = `#graphql
+  enum UserRole {
+    employee
+    team_lead
+    manager
+    hr
+    admin
+    super_admin
+  }
+
+  enum EmploymentType {
+    full_time
+    part_time
+    contractor
+    intern
+  }
+
+  enum UserStatus {
+    active
+    inactive
+    terminated
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    employeeId: String!
+    firstName: String!
+    lastName: String!
+    phone: String
+    dateOfBirth: String
+    department: ID
+    role: UserRole!
+    employmentType: EmploymentType!
+    joiningDate: String
+    status: UserStatus!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  input CreateUserInput {
+    email: String!
+    password: String!
+    employeeId: String!
+    firstName: String!
+    lastName: String!
+    phone: String
+    dateOfBirth: String
+    department: ID
+    role: UserRole
+    employmentType: EmploymentType
+    joiningDate: String
+    status: UserStatus
+  }
+
+  type Query {
+    _empty: String
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User!
+  }
+`;
--- backend/server.ts
+++ backend/server.ts
@@ -2,9 +2,33 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { userTypeDefs } from "./schema/userSchema.js";
+import { userResolver } from "./resolvers/userResolver.js";
+import { Query } from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+const server = new ApolloServer({
+  typeDefs: [userTypeDefs],
+
+  resolvers: {
+    Query: {
+      _empty: () => "GraphQL API running",
+    },
+    Mutation: {
+      ...userResolver.Mutation,
+    },
+  },
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server running at port: ${url}`);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
